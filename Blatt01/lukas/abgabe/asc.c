9b5b0f745835591f82119ae557604af22d824cd4
Lukas Koller <lukaskoller6@gmail.com>
2018-10-22 22:08:08 +0200

    fixed asc.c to pass asc_test

--- Blatt01/lukas/abgabe/asc.c
+++ Blatt01/lukas/abgabe/asc.c
@@ -6,6 +6,6 @@ int main()
     printf("Oct\tDec\tHex\tChar\n");
     for (int i = 0; i < 128; i++)
     {
-        printf("%03o\t%-i\t%02x\t%c\n",i, i, i, i);
+        printf("%03o\t%-3d\t%02x\t%c\n",i, i, i, i);
     }
 }
