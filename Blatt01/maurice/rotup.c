1997ac3208ef0f429735632bdfde0130f94f3a9d
mauricebrenner <mauricebrenner@github.com>
2018-10-26 00:52:14 +0200

    Fixed rotup.c

--- Blatt01/maurice/rotup.c
+++ Blatt01/maurice/rotup.c
@@ -7,15 +7,22 @@
  * A-Z : 65-90
  * a-z : 97-122
  * a + 32 = A
- * -52 = -'A' + 13
- * TODO Komischer Output für ABCDEFGHIJKLMNOPQRSTUVWXYZ -> NOPQRSTUVWXYZABCDEFGHIJKLM�
  */
 int main()
 {
     char input[256];
-    char rot13[256];
     read(0, input, 256);
-    for(int i = 0; i < strlen(input); i++) {
+    // Cut everything off after newline, because weird stuff happens afterwards
+    for(int i = 0; i < 256; i++) {
+        if(input[i] == '\n') {
+            input[i] = '\0';
+            break;
+        }
+    }
+
+    size_t length = strlen(input);
+    char *rot13 = malloc(length);
+    for(int i = 0; i < length; i++) {
         char c = input[i];
         if(c < 'A' || c > 'z' || (c < 'a' && c > 'Z')) {
             break;
@@ -26,12 +33,13 @@ int main()
         char rot13c = (char) ('A' + ((c - 'A' + 13) % 26));
         rot13[i] = rot13c;
     }
-    size_t len = strlen(input) + strlen(rot13) + 14;
+    size_t len = strlen(input) + strlen(rot13) + 13;
     char* output = malloc(len);
-    strcpy(output, "Hallo: ");
+    strcpy(output, "Hallo: "); // 7
     strcat(output, input);
-    strcat(output, " -- ");
+    strcat(output, " -- "); // 4
     strcat(output, rot13);
+    strcat(output, "\n"); // 2
     write(1, output, strlen(output));
     return 0;
 }
